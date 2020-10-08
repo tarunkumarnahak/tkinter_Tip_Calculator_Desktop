465fc8d8e07002b5f1f567c707eb1f20aa72528f
tarunkumarnahak <tarunkumarnahak89@gmail.com>
2020-10-09 01:20:34 +0530

    inital

---
+++ Tip_calculator.py
@@ -0,0 +1,100 @@
+# importing the modulus from tkinter
+from tkinter import Tk, Radiobutton, Button, Label, StringVar, IntVar, Entry
+
+# creating class and main application container
+
+
+class TipCalculator():
+    def __init__(self):
+        window = Tk()
+        window.title("Tip Calculator App")
+        window.configure(background="sky blue")
+        window.geometry("375x250")
+        window.resizable(width=False, height=False)
+
+        # creating variable
+        self.meal_cost = StringVar()
+        self.tip_percent = IntVar()
+        self.tip = StringVar()
+        self.total_cost = StringVar()
+
+        # creating Lables
+        tip_percent = Label(window, text='Tip percentage',
+                            bg="purple", fg="white")
+        tip_percent.grid(column=0, row=0, padx=15)
+
+        bill_amount = Label(window, text='Bill Amount', bg="black", fg="white")
+        bill_amount.grid(column=1, row=0, padx=15)
+
+        bill_amount_entry = Entry(
+            window, textvariable=self.meal_cost, width=14)
+        bill_amount_entry.grid(column=2, row=0)
+
+        # add radio buttons
+        five_percent_tip = Radiobutton(
+            window, text="0.5%", variable=self.tip_percent, value=5)
+        five_percent_tip.grid(column=0, row=1)
+
+        ten_percent_tip = Radiobutton(
+            window, text="10%", variable=self.tip_percent, value=10)
+        ten_percent_tip.grid(column=0, row=2)
+
+        fifteen_percent_tip = Radiobutton(
+            window, text="15%", variable=self.tip_percent, value=15)
+        fifteen_percent_tip.grid(column=0, row=3)
+
+        twenty_percent_tip = Radiobutton(
+            window, text="20%", variable=self.tip_percent, value=20)
+        twenty_percent_tip.grid(column=0, row=4)
+
+        twentyfive_percent_tip = Radiobutton(
+            window, text="25%", variable=self.tip_percent, value=25)
+        twentyfive_percent_tip.grid(column=0, row=5)
+
+        thirty_percent_tip = Radiobutton(
+            window, text="30%", variable=self.tip_percent, value=30)
+        thirty_percent_tip.grid(column=0, row=6)
+
+        # additional widgets
+        tip_amount_lb1 = Label(window, text="Tip Amount",
+                               bg='brown', fg='white')
+        tip_amount_lb1.grid(column=1, row=3, padx=15)
+        tip_amount_entry = Entry(window, textvariable=self.tip, width=14)
+        tip_amount_entry.grid(column=2, row=3)
+
+        bill_total_lb1 = Label(window, text="Bill Total",
+                               bg='blue', fg='white')
+        bill_total_lb1.grid(column=1, row=5, padx=15)
+        bill_total_entry = Entry(
+            window, textvariable=self.total_cost, width=14)
+        bill_total_entry.grid(column=2, row=5)
+
+        # buttons
+        calculate_btn = Button(window, text='Calculate',
+                               bg='green', fg='white', command=self.calculate)
+        calculate_btn.grid(column=1, row=7, padx=15)
+
+        clear_btn = Button(window, text='Clear', bg='black',
+                           fg='white', command=self.clear)
+        clear_btn.grid(column=2, row=7)
+        window.mainloop()
+
+     # calculate function
+
+    def calculate(self):
+        pre_tip = float(self.meal_cost.get())
+        percentage = self.tip_percent.get() / 100
+        tip_amount_entry = pre_tip*percentage
+        self.tip.set(tip_amount_entry)
+
+        final_bill = pre_tip+tip_amount_entry
+        self.total_cost.set(final_bill)
+
+    def clear(self):
+        self.total_cost.set("")
+        self.meal_cost.set("")
+        self.tip.set("")
+
+
+# calling the class
+TipCalculator()
